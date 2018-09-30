2c7c914e7d8c67f291bc53859104fa6a50751443
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-09-30 18:27:29 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS-1-Assignment/M3/Sorted_Array/Solution.java
+++ ADS-1-Assignment/M3/Sorted_Array/Solution.java
@@ -1,44 +1,80 @@
 import java.util.Scanner;
-import java.util.Arrays;
+
+/**
+ * Class for sorted array.
+ */
 class SortedArray {
-    private int[] firstArray;
-    private int[] secondArray;
+    /**
+     * firstarray.
+     */
+    private int[] firstarray;
+    /**
+     * seconfarray.
+     */
+    private int[] secondarray;
+    /**
+     * Constructs the object.
+     * @param      a1    A 1
+     * @param      a2    A 2
+     */
     SortedArray(final int[] a1, final int[] a2) {
-        this.firstArray = a1;
-        this.secondArray = a2;
+        this.firstarray = a1;
+        this.secondarray = a2;
     }
-    public String sortOf(int[] a1, int[] a2) {
-        int i = 0, j = 0, k = 0;
-        int[] resultArray = new int[a1.length + a2.length];
-        while (i < a1.length && j < a2.length) {
-            if(a1[i] < a2[j]) {
-                resultArray[k++] = a1[i++];
+    /**
+     * getting sorted elements.
+     * @param      arr1  The arr 1
+     * @param      arr2  The arr 2
+     *
+     * @return   sorted array
+     */
+    public String sort(final int[] arr1, final int[] arr2) {
+        int i = 0;
+        int j = 0, k = 0;
+        int[] array = new int[arr1.length + arr2.length];
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                array[k++] = arr1[i++];
             } else {
-                resultArray[k++] = a2[j++];
+                array[k++] = arr2[j++];
             }
         }
-        while (i < a1.length) {
-            resultArray[k++] = a1[i++];
+        while (i < arr1.length) {
+            array[k++] = arr1[i++];
         }
-        while (j < a2.length) {
-            resultArray[k++] = a2[j++];
+        while (j < arr2.length) {
+            array[k++] = arr2[j++];
         }
-        String result = "";
-        // int index = 0;
-        for (int index = 0; index < resultArray.length; k++) {
-            result += resultArray[index] + ",";
-        }
-        return result;
+        String res = "";
+        int index = 0;
+        for (index = 0; index < array.length - 1; index++) {
+            res += array[index] + ",";
+        } res += array[index];
+        return res;
     }
-
 }
-class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size1 = Integer.parseInt(sc.nextLine());
-        int size2 = Integer.parseInt(sc.nextLine());
-        String[] arraytemp1 = sc.nextLine().split(",");
-        String[] arraytemp2 = sc.nextLine().split(",");
+
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size1 = Integer.parseInt(scan.nextLine());
+        int size2 = Integer.parseInt(scan.nextLine());
+        String[] arraytemp1 = scan.nextLine().split(",");
+        String[] arraytemp2 = scan.nextLine().split(",");
         int[] array1 = new int[size1];
         int[] array2 = new int[size2];
         for (int i = 0; i < size1; i++) {
@@ -47,10 +83,8 @@ class Solution {
         for (int j = 0; j < size2; j++) {
             array2[j] = Integer.parseInt(arraytemp2[j]);
         }
-        SortedArray saobj = new SortedArray(array1, array2);
-        System.out.println(saobj.sortOf(array1, array2));
-
-
+        SortedArray sa = new SortedArray(array1, array2);
+        System.out.println(sa.sort(array1, array2));
 
     }
 }

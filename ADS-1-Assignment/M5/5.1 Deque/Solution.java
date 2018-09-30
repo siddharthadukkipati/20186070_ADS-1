4b7ea8ab4694955dd0b56290d50952eceba35da6
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-09-30 18:21:14 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS-1-Assignment/M5/5.1 Deque/Solution.java
+++ ADS-1-Assignment/M5/5.1 Deque/Solution.java
@@ -1,165 +1,193 @@
 import java.util.Scanner;
 /**
-*class deque which is a generic type.
-*/
-class Deque<Item> {
-	Node left = null;
-	Node right = null;
-	int size = 0;
-	/**
-	*Class node.
-	*/
-	class Node {
-		Item data;
-		Node next;
-		/**
-		*constructor.
-		*@param inputdata input data.
-		*/
-		Node(Item inputdata) {
-			this.data = inputdata;
-		}
-	}
-	/**
-	*checks if the queue is empty.
-	*@return true or false.
-	*/
-	boolean isEmpty() {
-		return size == 0;
-	}
-	/**
-	*This method returns the size of the queue.
-	*@return size.
-	*/
-	int size() {
-		return size;
-	}
-	/**
-	*This method pushes the immediate right element in a given queue.
-	*/
-	void pushLeft(Item inputitem) {
-		Node newnode = new Node(inputitem);
-		if(isEmpty()) {
-			left = newnode;
-			right = left;
-			size++;
-			print();
-			return;
-		}
-		newnode.next = left;
-		left = newnode;
-		size++;
-		print();
-	}
-	/**
-	*This method pushes the immediate right element in a given queue.
-	*/
-	void pushRight(Item inputitem) {
-		if (isEmpty()) {
-			pushLeft(inputitem);
-			return;
-		}
-		if (size == 1) {
-			left = right;
-		}
-		Node newnode = new Node(inputitem);
-		right.next = newnode;
-		right = newnode;
-		size++;
-		print();
-	}
-	/**
-	*This method removes the immediate left element in a given queue.
-	*/
-	public Item popLeft() {
-		if (isEmpty()) {
-			System.out.println("Deck is empty");
-			return null;
-		}
-		Item leftout = left.data;
-		left = left.next;
-		size--;
-		print();
-		return leftout;
-	}
-	/**
-	*This method removes the immediate right element in a given queue.
-	*/
-	public Item popRight() {
-		if (isEmpty()) {
-			System.out.println("Deck is empty");
-			return null;
-		}
-		Item rightout = right.data;
-		Node test = left;
-		int counter = 1;
-		while (counter < size - 1) {
-			test = test.next;
-			counter++;
-		}
-		right = test;
-		right.next = null;
-		size--;
-		print();
-		return rightout;
-	}
-	/**
-	*this method is used to print.
-	*/
-    public void print() {
-    	if (isEmpty()) {
-    		System.out.println("[]");
-    		return;
-    	}
-    	String str = "";
-    	str = str + "[";
-    	Node test = left;
-    	while(test.next != null) {
-    		str = str + test.data + ", ";
-    		test = test.next;
-    	}
-    	str = str + right.data;
-    	str = str + "]";
-    	System.out.println(str);
+ * Class for deque.
+ */
+class Deque {
+    /**
+     * variable.
+     */
+    private Node first;
+    /**
+     * last variable.
+     */
+    private Node last;
+    /**
+     * count variable.
+     */
+    private int count;
+    /**
+     * Constructs the object.
+     */
+    Deque() {
+        first = null;
+        last = null;
+        count = 0;
+    }
+
+    /**
+     * Class for node.
+     */
+    class Node {
+        /**
+         * variable.
+         */
+        private int item;
+        /**
+         * variable.
+         */
+        private Node next;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+    /**
+     * size.
+     *
+     * @return count.
+     */
+    public int size() {
+        return count;
+    }
+    /**
+     * Pushes a left.
+     *
+     * @param      item  The item
+     */
+    public void pushLeft(final int item) {
+        if (first == null) {
+            //Node oldfirst = first;
+            first = new Node();
+            first.item = item;
+            first.next = null;
+            last = first;
+        } else {
+            Node oldlast = first;
+            first = new Node();
+            first.item = item;
+            first.next = oldlast;
+        }
+        count++;
+    }
+    /**
+     * Pushes a right.
+     *
+     * @param      item  The item
+     */
+    public void pushRight(final int item) {
+        if (last == null) {
+            last = new Node();
+            last.item = item;
+            last.next = null;
+            first = last;
+        }   else {
+            Node temp = last;
+            last = new Node();
+            last.item = item;
+            last.next = null;
+            temp.next = last;
+        }
+        count++;
+    }
+    /**
+     * pop from left.
+     */
+    public void popLeft() {
+        if (first != null) {
+            first = first.next;
+            count--;
+        }
+    }
+    /**
+     * pop from right.
+     */
+    public void popRight() {
+        if (last != null) {
+            Node temp = first;
+            while (temp.next.next != null) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            last = temp;
+            count--;
+        }
+    }
+    /**
+     * to string to display.
+     *
+     * @return  string.
+     */
+    public String display() {
+        if (count != 0) {
+            String s = "[";
+            Node temp = first;
+            while (temp != null) {
+                s += temp.item + ", ";
+                temp = temp.next;
+            }
+            return s.substring(0, s.length() - 2) + "]";
+        }
+        return "[]";
     }
 }
 /**
-*This class is to read the input from the testcases.
-*/
-class Solution {
-	/**
-	*deafult constructor.
-	*/
-	Solution() {
-	}
-	/**
-	*main method is used to read the input.
-	*@param args passing the arguments.
-	*/
-	public static void main(final String[] args) {
-		Deque<Integer> dequeobj = new Deque<Integer>();
-		Scanner scan = new Scanner(System.in);
-		while (scan.hasNext()) {
-			String line = scan.nextLine();
-			String[] tokens = line.split(" ");
-			switch (tokens[0]) {
-			case "pushLeft":
-				dequeobj.pushLeft(Integer.parseInt(tokens[1]));
-				break;
-			case "pushRight":
-				dequeobj.pushRight(Integer.parseInt(tokens[1]));
-				break;
-			case "size":
-				System.out.println(dequeobj.size());
-				break;
-			case "popRight":
-				dequeobj.popRight();
-				break;
-			case "popLeft":
-				dequeobj.popLeft();
-				break;
-			default:
-			}
-		}
-	}
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     *constructs object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        Deque link = new Deque();
+        n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] line = sc.nextLine().split(" ");
+            switch (line[0]) {
+            case "pushLeft":
+                link.pushLeft(Integer.parseInt(line[1]));
+                System.out.println(link.display());
+                break;
+            case "pushRight":
+                link.pushRight(Integer.parseInt(line[1]));
+                System.out.println(link.display());
+                break;
+            case "popRight":
+                if (!link.isEmpty()) {
+                    link.popRight();
+                    System.out.println(link.display());
+                } else {
+                    System.out.println("Deck is empty");
+                }
+                break;
+            case "popLeft":
+                if (!link.isEmpty()) {
+                    link.popLeft();
+                    System.out.println(link.display());
+                } else {
+                    System.out.println("Deck is empty");
+                }
+
+                break;
+            case "size":
+                System.out.println(link.size());
+            default:
+                break;
+            }
+
+        }
+    }
 }

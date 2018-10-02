09c45b32b01ab3f19305f804065cc52ea67f057b
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-10-02 18:58:55 +0530

    "check_style_score -> 2 of 6 passed. style score: 0/1 "

--- ADS-1-Assignment/M8/8.2 Team Ranking With Selection/Solution.java
+++ ADS-1-Assignment/M8/8.2 Team Ranking With Selection/Solution.java
@@ -28,9 +28,9 @@ class Tournament {
 			return 1;
 		else if (this.getTeamWins() < that.getTeamWins())
 			return -1;
-		if (this.getTeamLooses() < that.getTeamLooses())
+		if (this.getTeamLooses() > that.getTeamLooses())
 			return 1;
-		else if (this.getTeamLooses() > that.getTeamLooses())
+		else if (this.getTeamLooses() < that.getTeamLooses())
 			return -1;
 		if (this.getTeamDraws() > that.getTeamDraws())
 			return 1;

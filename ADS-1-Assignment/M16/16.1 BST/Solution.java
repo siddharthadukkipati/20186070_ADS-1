b48f09de035dc47a42ff2974083d21f615a25a34
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-10-11 17:50:39 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignment/M16/16.1 BST/Solution.java
+++ ADS-1-Assignment/M16/16.1 BST/Solution.java
@@ -1,121 +1,217 @@
 import java.util.Scanner;
+/**
+ * Class for book details.
+ */
 class BookDetails implements Comparable {
-	private String name;
-	private String author;
-	private float price;
-
-	BookDetails(String bookname, String bookauthor, float bookprice) {
-		this.name = bookname;
-		this.author = bookauthor;
-		this.price = bookprice;
-	}
-	public String getName() {
-		return this.name;
-	}
-	public String getAuthor() {
-		return this.author;
-	}
-	public float getPrice() {
-		return this.price;
-	}
-	public int compareTo(Object object) {
-		BookDetails that = (BookDetails) object;
-		return this.name.compareTo(that.name);
-	}
+    /**
+     * { var_description }
+     */
+    private String name;
+    /**
+     * { var_description }
+     */
+    private String author;
+    /**
+     * { var_description }
+     */
+    private float price;
+    /**
+     * Constructs the object.
+     *
+     * @param      bookname    The bookname
+     * @param      bookauthor  The bookauthor
+     * @param      bookprice   The bookprice
+     */
+    BookDetails(String bookname, String bookauthor, float bookprice) {
+        this.name = bookname;
+        this.author = bookauthor;
+        this.price = bookprice;
+    }
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return this.name;
+    }
+    /**
+     * Gets the author.
+     *
+     * @return     The author.
+     */
+    public String getAuthor() {
+        return this.author;
+    }
+    /**
+     * Gets the price.
+     *
+     * @return     The price.
+     */
+    public float getPrice() {
+        return this.price;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      object  The object
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(Object object) {
+        BookDetails that = (BookDetails) object;
+        return this.name.compareTo(that.name);
+    }
 }
-
-
+/**
+ * Class for node.
+ */
 class Node {
-	public BookDetails key;
-	public int val;
-	public Node left, right;
-	//private int size;
-
-	public Node(BookDetails key, int val) {
-		this.key = key;
-		this.val = val;
-		left = null;
-		right = null;
-		//this.size = size;
-	}
+    /**
+     * { var_description }
+     */
+    public BookDetails key;
+    /**
+     * { var_description }
+     */
+    public int val;
+    /**
+     * { item_description }
+     */
+    public Node left, right;
+    /**
+     * Constructs the object.
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
+    public Node(BookDetails key, int val) {
+        this.key = key;
+        this.val = val;
+        left = null;
+        right = null;
+        //this.size = size;
+    }
 }
-
+/**
+ * Class for binary search tree.
+ */
 class BinarySearchTree {
-	//BookDetails dobj = new BookDetails();
-	private Node root;
-
-	BinarySearchTree() {
-		root = null;
-	}
-	public Node root() {
-		return root;
-	}
-	public int get(BookDetails key) {
-		//System.out.println("hello");
-		Node x = root;
-		while (x != null) {
-			int cmp = key.compareTo(x.key);
-			if (cmp < 0) {
-				x = x.left;
-			} else if (cmp > 0) {
-				x = x.right;
-			} else if (cmp == 0) {
-				return x.val;
-			}
-		}
-		return -1;
-	}
-	public void put(BookDetails key, int val) {
-		//System.out.println(key.getName()+"put 2 ints "+val);
-		root = put(root, key, val);
-	}
-	private Node put(Node x, BookDetails key, int val) {
-		if (x == null) {
-			return new Node(key, val);
-			//break;
-		}
-		// System.out.println(key+" "+x.key);
-		int cmp = key.compareTo(x.key);
-		//System.out.println(cmp);
-		if (cmp < 0) {
-			x.left = put(x.left, key, val);
-		} else if (cmp > 0) {
-			x.right = put(x.right, key, val);
-		} else if (cmp == 0) {
-			x.val = val;
-			//System.out.println("Replaced");
-		}
-		return x;
-	}
-
-
+    /**
+     * { var_description }
+     */
+    private Node root;
+    /**
+     * Constructs the object.
+     */
+    BinarySearchTree() {
+        root = null;
+    }
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Node root() {
+        return root;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int get(BookDetails key) {
+        //System.out.println("hello");
+        Node x = root;
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) {
+                x = x.left;
+            } else if (cmp > 0) {
+                x = x.right;
+            } else if (cmp == 0) {
+                return x.val;
+            }
+        }
+        return -1;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
+    public void put(BookDetails key, int val) {
+        //System.out.println(key.getName()+"put 2 ints "+val);
+        root = put(root, key, val);
+    }
+    /**
+     * { function_description }
+     *
+     * @param      x     { parameter_description }
+     * @param      key   The key
+     * @param      val   The value
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node put(Node x, BookDetails key, int val) {
+        if (x == null) {
+            return new Node(key, val);
+            //break;
+        }
+        // System.out.println(key+" "+x.key);
+        int cmp = key.compareTo(x.key);
+        //System.out.println(cmp);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else if (cmp == 0) {
+            x.val = val;
+            //System.out.println("Replaced");
+        }
+        return x;
+    }
 }
+/**
+ * { item_description }
+ */
 public final class Solution {
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		BinarySearchTree bstobj = new BinarySearchTree();
-		while (sc.hasNextLine()) {
-			String[] tokens = sc.nextLine().split(",");
-			switch (tokens[0]) {
-			case "put":
-				BookDetails deobj = new BookDetails(tokens[1], tokens[2], Float.parseFloat(tokens[2 + 1]));
-				bstobj.put(deobj, Integer.parseInt(tokens[2 + 2]));
-				break;
-			case "get":
-				//bstobj.get(deobj);
-				deobj = new BookDetails(tokens[1], tokens[2], Float.parseFloat(tokens[2 + 1]));
-				if (bstobj.get(deobj) == -1) {
-					System.out.println("null");
-				} else {
-					System.out.println(bstobj.get(deobj));
-				}
-				break;
-			default:
-				break;
-			}
-		}
-	}
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTree bstobj = new BinarySearchTree();
+        while (sc.hasNextLine()) {
+            String[] tokens = sc.nextLine().split(",");
+            switch (tokens[0]) {
+            case "put":
+                BookDetails deobj = new BookDetails(tokens[1], tokens[2], Float.parseFloat(tokens[2 + 1]));
+                bstobj.put(deobj, Integer.parseInt(tokens[2 + 2]));
+                break;
+            case "get":
+                //bstobj.get(deobj);
+                deobj = new BookDetails(tokens[1], tokens[2], Float.parseFloat(tokens[2 + 1]));
+                if (bstobj.get(deobj) == -1) {
+                    System.out.println("null");
+                } else {
+                    System.out.println(bstobj.get(deobj));
+                }
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }

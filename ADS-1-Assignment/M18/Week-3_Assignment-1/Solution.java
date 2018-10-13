2fa7cc49fb3c92ff5086aefbc8cfaed3adb2c71d
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-10-13 12:37:08 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignment/M18/Week-3_Assignment-1/Solution.java
+++ ADS-1-Assignment/M18/Week-3_Assignment-1/Solution.java
@@ -1,18 +1,50 @@
 import java.util.Scanner;
+/**
+ * Class for stock.
+ */
 class Stock implements Comparable<Stock> {
+    /**
+     * { var_description }.
+     */
     private String stockName;
+    /**
+     * { var_description }.
+     */
     private float stockChange;
-    Stock(String name, float change) {
+    /**
+     * Constructs the object.
+     *
+     * @param      name    The name
+     * @param      change  The change
+     */
+    Stock(final String name, final float change) {
         this.stockName = name;
         this.stockChange = change;
     }
+    /**
+     * Gets the stock name.
+     *
+     * @return     The stock name.
+     */
     public String getStockName() {
         return this.stockName;
     }
+    /**
+     * Gets the stock change.
+     *
+     * @return     The stock change.
+     */
     public float getStockChange() {
         return this.stockChange;
     }
-    public int compareTo(Stock other) {
+    /**
+     * { function_description }.
+     *
+     * @param      other  The other
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Stock other) {
         if (this.stockChange > other.stockChange) {
             return 1;
         }
@@ -27,28 +59,49 @@ class Stock implements Comparable<Stock> {
         }
         return 0;
     }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         String inputStr = "";
         inputStr = inputStr + this.stockName + " " + this.stockChange;
         return inputStr;
     }
 }
-class Solution {
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         //unused.
     }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-        BinarySearchTree<String, Integer> maxST = new  BinarySearchTree<>();
-        BinarySearchTree<String, Integer> minST = new BinarySearchTree<>();
+        BinarySearchTree<String, Integer> maxST
+        = new  BinarySearchTree<>();
+        BinarySearchTree<String, Integer> minST
+        = new BinarySearchTree<>();
         int stockLength = Integer.parseInt(scan.nextLine());
-        for (int i = 0; i < 6; i++) {
+        final int six = 6;
+        final int five = 5;
+        for (int i = 0; i < six; i++) {
             int count = 0;
             MinPQ<Stock> minpqObj = new MinPQ<>();
             MaxPQ<Stock> maxpqObj = new MaxPQ<>();
             while (count < stockLength) {
                 String[] tokens = scan.nextLine().split(",");
-                Stock stockObj = new Stock(tokens[0], Float.parseFloat(tokens[1]));
+                Stock stockObj = new Stock(tokens[0],
+                    Float.parseFloat(tokens[1]));
                 minpqObj.insert(stockObj);
                 maxpqObj.insert(stockObj);
                 count++;
@@ -80,12 +133,12 @@ class Solution {
                     minST.put(minimumStock.getStockName(), 0);
                 }
             }
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < five; j++) {
                 Stock maxPQBest = maxpqObj.delMax();
                 System.out.println(maxPQBest);
             }
             System.out.println();
-            for (int k = 0; k < 5; k++) {
+            for (int k = 0; k < five; k++) {
                 Stock minPQWorst = minpqObj.delMin();
                 System.out.println(minPQWorst);
             }

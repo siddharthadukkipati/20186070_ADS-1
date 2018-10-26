8455addac8de88e263923b84221241ccf18be8e6
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-10-26 17:41:41 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-Assignment/M23/23.1 Hashing with Linear Probing/Solution.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // Constructor.
+    }
+    /**
+     * {Client Program}.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        LinearProbingHashST<String, Integer> hash;
+        int lines = Integer.parseInt(scan.nextLine());
+        hash = new LinearProbingHashST<String, Integer>();
+        while (lines > 0) {
+            String[] tokens = scan.nextLine().split(" ");
+            switch (tokens[0]) {
+            case "put":
+                hash.put(tokens[1], Integer.parseInt(tokens[2]));
+                break;
+            case "get":
+                System.out.println(hash.get(tokens[1]));
+                break;
+            case "delete":
+                hash.delete(tokens[1]);
+                break;
+            case "display":
+                try {
+                    hash.display();
+                } catch (Exception e) {
+                    System.out.println("{}");
+                }
+                break;
+            default:
+                break;
+            }
+            lines--;
+        }
+    }
+}

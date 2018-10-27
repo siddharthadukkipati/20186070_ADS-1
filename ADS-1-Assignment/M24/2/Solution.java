fdef06b349b9c64bdcc9766b19db9e331cab2449
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-10-27 12:54:25 +0530

    "m24_a2_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignment/M24/2/Solution.java
+++ ADS-1-Assignment/M24/2/Solution.java
@@ -1,22 +1,66 @@
+/**
+ * { item_description }.
+ */
 import java.util.Scanner;
+/**
+ * Class for student data.
+ */
 class StudentData implements Comparable<StudentData> {
+    /**
+     * { var_description }.
+     */
     private String name;
+    /**
+     * { var_description }.
+     */
     private double total;
+    /**
+     * { var_description }.
+     */
     private int regNumber;
+    /**
+     * Constructs the object.
+     *
+     * @param      name1       The name 1
+     * @param      total1      The total 1
+     * @param      regnumber1  The regnumber 1
+     */
     StudentData(final String name1, final String total1, final String regnumber1) {
         this.name  = name1;
         this.total = Double.parseDouble(total1);
         this.regNumber = Integer.parseInt(regnumber1);
     }
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
     public String getName() {
         return name;
     }
+    /**
+     * Gets the total.
+     *
+     * @return     The total.
+     */
     public double getTotal() {
         return total;
     }
+    /**
+     * Gets the roll no.
+     *
+     * @return     The roll no.
+     */
     public int getRollNo() {
         return regNumber;
     }
+    /**
+     * { function_description }.
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int compareTo(final StudentData that) {
         if (this.total > that.total) {
             return 1;
@@ -33,10 +77,21 @@ class StudentData implements Comparable<StudentData> {
         return 0;
     }
 }
+/**
+ * { item_description }.
+ */
 public final class Solution {   
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         //unused.
     }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int lines = Integer.parseInt(scan.nextLine());
